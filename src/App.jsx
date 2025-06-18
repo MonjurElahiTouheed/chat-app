7e94a66328e33476954ee9982f90688f17a830cf
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-06-18 12:41:33 +0600

    registration page designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
-
+import register from './assets/login-image.png';
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <div className="flex">
+            <div className="w-[60%] pt-[225px] pl-[0px]">
+              <h2 className="font-secondary font-bold text-secondary text-[34px]">
+                Get started with easily register
+              </h2>
+              <p className='font-secondary text-[20px] text-black/50 mt-[13px]'>Free register and you can enjoy it</p>
+        </div>
+        <div className='w-[40%]'>
+            <img src={register} alt="" />
+        </div>
+      </div>
     </>
   )
 }
