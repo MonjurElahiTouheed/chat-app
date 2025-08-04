fa74017c37700470a8f34e7888238de9197257bf
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-08-04 11:35:20 +0600

    design chatbox

--- src/components/Chatbox/Chatbox.jsx
+++ src/components/Chatbox/Chatbox.jsx
@@ -8,7 +8,7 @@ const Chatbox = () => {
         <div className="w-[689px h-[956px shadow-[0_0px_3px_rgba(0,0,0,0.25)] rounded-[20px] flex flex-col">
 
             {/* chatbox top header */}
-            <Flex className='rounded-t-[20px] px-[30px] py-[27px] shadow-[0_1px_3px_rgba(0,0,0,0.25)]'>
+            <Flex className='rounded-t-[20px] px-[30px] py-[27px] shadow-[0_-1px_3px_rgba(0,0,0,0.25)]'>
                 {/* profile info */}
                 <div className='flex items-center gap-[25px]'>
                     <div>
@@ -34,9 +34,11 @@ const Chatbox = () => {
                 </div>
             </div>
 
-            <div className="px-6 pb-[20px rounded-b-[20px] shadow-[0_-1px_3px_rgba(0,0,0,0.25)] flex items-cente">
-                <input type="text" className="w-full pl-2 py-4 outline-none mt- bg-[#F1F1F1] rounded-[10px]" placeholder="send your mind" />
-                <Button className='px- py-'><GrSend size={20}/></Button>
+            <div className="px-7 py-[20px] rounded-b-[20px] shadow-[0_-1px_3px_rgba(0,0,0,0.25)] flex items-center gap-5">
+                <input type="text" className="w-full pl-5 py-4 outline-none bg-[#F1F1F1] rounded-[10px]" placeholder="send your mind" />
+                <div>
+                    <button className='px-2 py-2 mt-2'><GrSend size={30}/></button>
+                </div>
             </div>
 
         </div>
